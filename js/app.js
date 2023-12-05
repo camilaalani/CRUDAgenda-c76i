85c6dcff7b2e2abcf585de756efc9b0169fecd76
Camila Alani <alanicamila124@gmail.com>
2023-12-05 03:55:56 -0300

    agregamos un modelo de como creariamos el objeto contacto

--- js/app.js
+++ js/app.js
@@ -1,6 +1,32 @@
 import Contacto from "./classContacto.js";
 
-console.log("prueba desde app.js");
+//const contacto = new Contacto(1, 'Juan', 'Pérez', 'juan.perez@email.com', '555-123-4567');
 
-const contacto = new Contacto(1, 'Juan', 'Pérez', 'juan.perez@email.com', '555-123-4567');
-console.log(contacto);
+const modalAdminContacto = new bootstrap.Modal(document.getElementById("administrarContacto"));
+const btnAgregarContacto = document.getElementById("btnNuevoContacto");
+const formularioContacto = document.querySelector("form");
+const nombre = document.getElementById("nombre"),
+apellido = document.getElementById("apellido"),
+telefono = document.getElementById("telefono"),
+email = document.getElementById("email");
+
+//funciones
+const mostrarModal = ()=>{
+    modalAdminContacto.show();
+}
+
+const crearContacto = (e) =>{
+    e.preventDefault();
+    console.log("aqui debo crear el contacto nuevo");
+    //verificar que los datos sean validos
+
+    //crearia el contacto
+    const nuevoContacto = new Contacto(undefined, nombre.value, apellido.value, email.value, telefono.value);
+    console.log(nuevoContacto);
+
+    //resetear el formulario
+}
+
+//logica extra
+btnAgregarContacto.addEventListener("click", mostrarModal);
+formularioContacto.addEventListener("submit", crearContacto);
