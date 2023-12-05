bb519d603a5c81f245ebaa27e23d6b829862d733
Camila Alani <alanicamila124@gmail.com>
2023-12-05 17:47:52 -0300

    borramos un contacto del array agenda y actualizo el localstorage

--- js/app.js
+++ js/app.js
@@ -69,7 +69,7 @@ function crearFila(contacto, fila) {
     <td>${contacto.celular}</td>
     <td>
       <button class="btn btn-warning">Editar</button
-      ><button class="btn btn-danger">Borrar</button>
+      ><button class="btn btn-danger" onclick="borrarContacto('${contacto.id}')">Borrar</button>
     </td>
   </tr>`;
 }
@@ -96,6 +96,20 @@ function cargaInicial() {
     //  }
   }
   //agregar un cartel informativo para el usuario
+}
+
+
+window.borrarContacto = (idContacto)=>{
+  console.log("desde la funcion borrar contacto");
+  console.log(idContacto);
+  //buscar en el array el objeto que tiene este idContacto array.findIndex
+  const posicionContactoBuscado = agenda.findIndex((itemcontacto)=> itemcontacto.id === idContacto);
+  console.log(posicionContactoBuscado)
+  //borrar el objeto del array usando splice(posicion del objeto, cuantos borro)
+  agenda.splice(posicionContactoBuscado, 1);
+  //actualizar el localstorage
+  guardarEnLocalStorage();
+  //borrar una fila de la tabla
 }
 
 //logica extra
