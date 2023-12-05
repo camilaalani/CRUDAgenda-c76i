e8bf9b7e6a3989113c740d807371b922e3995739
Camila Alani <alanicamila124@gmail.com>
2023-12-05 03:02:13 -0300

    agregamos la clase contacto y la usamos desde app.js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+import Contacto from "./classContacto.js";
+
+console.log("prueba desde app.js");
+
+const contacto = new Contacto(1, 'Juan', 'Pérez', 'juan.perez@email.com', '555-123-4567');
+console.log(contacto);
